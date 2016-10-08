ff151f10278d5e9a7965b72e2b3b97cf7bbcba90
gnox <gnox@klapptux>
2016-10-08 03:30:11 +0200

    cleanup

--- rovy_server/scripts/start_server.sh
+++ rovy_server/scripts/start_server.sh
@@ -7,13 +7,11 @@ amixer cset numid=3 1
 # volume
 amixer cset numid=1 -- 100%
 
-# load rasberry camera v4l driver (needed for opencv) 
+# load raspberry camera v4l driver (needed for opencv) 
 modprobe bcm2835-v4l2
 
 export CLASSPATH=pi4j-core.jar:pi4j-core-javadoc.jar:pi4j-core-sources.jar:pi4j-device.jar:pi4j-device-javadoc.jar:pi4j-device-sources.jar:pi4j-example.jar:pi4j-example-javadoc.jar:pi4j-example-sources.jar:pi4j-gpio-extension.jar:pi4j-gpio-extension-javadoc.jar:pi4j-gpio-extension-sources.jar:pi4j-service.jar:pi4j-service-javadoc.jar:pi4j-service-sources.jar:pi4j-1.1-SNAPSHOT/lib/:rovy_opencvwrapper-0.0.1-SNAPSHOT.jar:rovy_api-0.0.1-SNAPSHOT.jar:rovy_server-0.0.1-SNAPSHOT.jar
-#export CLASSPATH=rasp_rover_server-0.0.1-SNAPSHOT.jar
 
-echo $CLASSPATH
 killall rmiregistry
 rmiregistry 1234 &
 java -Djava.library.path=. -Djava.security.policy=./rovysecurity.policy -classpath $CLASSPATH de.gnox.rovy.server.Launcher
